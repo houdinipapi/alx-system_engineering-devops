ca99c3384a78b083aac6d6c09c8e48f6b92c3f84
houdinipapi <ochiengcliff.co@gmail.com>
2023-08-17 10:00:21 +0000

    User limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Enable user "holberton" to log in and open files without errors
+
+# Increasing hard file limit
+exec { 'increase-hard-file-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increasing soft file limit
+exec { 'increase-soft-file-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
